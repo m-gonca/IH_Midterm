a151c8135e500e89ab395d3d09762750514213d3
m-gonca <monicagonca1992@gmail.com>
2022-08-05 11:13:30 +0200

    all

--- contact.js
+++ contact.js
@@ -21,5 +21,29 @@ function addPost (preventForm) {
 		body: JSON.stringify({name: name, email: email, phone: phone, message: message}),
 		})
 	.then((response) => response.json())
-	.then((dataDeFormulario) => console.log(dataDeFormulario));
+	.then((dataDeFormulario) => console.log(dataDeFormulario))
+	.then(() => {
+		let message = "";
+		message += `
+		<div id="message2">
+			<h6>Yeh I got ya fren!</h6>
+		</div>
+		`;
+		document.getElementById("message3").innerHTML = message;
+		console.log(document.getElementById("message3"));
+	})
+	.catch(() => {
+		let message = "";
+		message += `
+		<div id="message2">
+			<h6>Oops, I did it again, I played with your posts, got lost in the fetch</h6>
+		</div>
+		`;
+		document.getElementById("message3").innerHTML = message;
+	});
+
+	document.getElementById("name").value = "";
+	document.getElementById("email").value = "";
+	document.getElementById("phone").value = "";
+	document.getElementById("message").value = "";
 }
